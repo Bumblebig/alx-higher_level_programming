444fa8fc6df33cf84478bf08ce2e30272170d117
Abdurrahman <dreydare16@gmail.com>
2023-03-15 22:48:32 +0100

    fixing errors

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dic = {}
+    new_dict = {}
     for k, v in a-dictionary.values():
-        new_dic[k] = v * 2
-    return new_dic
+        new_dict[k] = v * 2
+    return new_dict
