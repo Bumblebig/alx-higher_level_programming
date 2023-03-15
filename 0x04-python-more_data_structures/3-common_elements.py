06c5bc59dee26da2d3a0651ebab0499a263ca2b9
Abdurrahman <dreydare16@gmail.com>
2023-03-15 15:28:04 +0100

    fixed incorrect variable names error

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_list = []
+    new_set = {} 
     for item in set_1:
         if item in set_2:
-            new_list.append(item)
-    return set(new_list)
+            new_set.add(item)
+    return new_set
     
