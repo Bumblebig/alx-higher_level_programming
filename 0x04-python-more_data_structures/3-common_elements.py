2e87ab916e7e38e30754529094db7ea4b5c4c3fc
Abdurrahman <dreydare16@gmail.com>
2023-03-15 15:24:48 +0100

    fixing errors

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    new_set = set_1.union(set_2)
-    return set(new_set)
+    new_list = []
+    for item in set_1:
+        if item in set_2:
+            new_list.append(item)
+    return set(new_list)
+    
