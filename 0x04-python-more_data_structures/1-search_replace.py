c511df91fec7aa7bd9e7564eba3112b69714319c
Abdurrahman <dreydare16@gmail.com>
2023-03-15 13:07:52 +0100

    uploading solution

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     i = 0
     for item in my_list:
         if item == search:
-            my_list[i] = replace
+            item[i] = replace
     i += 1
+    return my_list
