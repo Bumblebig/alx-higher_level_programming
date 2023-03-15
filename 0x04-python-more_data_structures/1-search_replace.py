b2e4e8d00a1e91ff70ba2e7fe0692d9b535e3b43
Abdurrahman <dreydare16@gmail.com>
2023-03-15 13:27:50 +0100

    Fixing syntax error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,8 @@ def search_replace(my_list, search, replace):
     new_list = []
     i = 0
     for item in my_list:
+        new_list.append(item)
         if item == search:
-            item[i] = replace
+            new_list[i] = search
         i += 1
     return new_list
