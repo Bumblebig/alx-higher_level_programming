1fae389dc3ea090003d94aad38e68271d27dfc2e
Abdurrahman <dreydare16@gmail.com>
2023-03-15 15:40:01 +0100

    fixed logical error

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,6 +2,8 @@
 def only_diff_elements(set_1, set_2):
     new_set = []
     for item in set_1:
-        if item not in set_1 and item not in set_2:
+        if item in set_1 and item in set_2:
+            item = item
+        else:
             new_set.append(item)
     return set(new_set)
