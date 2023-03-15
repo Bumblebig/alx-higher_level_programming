ff389a08905d20468b36c3a32a3c92f46a958b93
Abdurrahman <dreydare16@gmail.com>
2023-03-15 23:30:38 +0100

    solution update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best_key = 0
-    keyVal = ''
-    message = "Best score: "
-    if a_dictionary:
-        for key, val in a_dictionary.items():
-            if val > best_key:
-                best_key = val
-                keyVal = key
-        print(f"{message}{keyVal}")
-    else:
-        print(f"{message}{None}")
+    if a_dictionary and len(a_dictionary):
+        max = list(a_dictionary.keys())[0]
+        for key in a_dictionary:
+            if a_dictionary[key] > a_dictionary[max]:
+                max = key
+        return max
+    return None
