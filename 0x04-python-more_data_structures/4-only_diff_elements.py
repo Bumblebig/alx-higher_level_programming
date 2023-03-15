1d5438a21f7b6672370373d7b45351f663ffdc2e
Abdurrahman <dreydare16@gmail.com>
2023-03-15 15:55:27 +0100

    restructurd the logical statements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_set = []
-    for item in set_1:
-        if item not in set_1 and item not in set_2:
-            new_set.append(item)    
-    return set(new_set)
+    return set_1 ^ set_2
