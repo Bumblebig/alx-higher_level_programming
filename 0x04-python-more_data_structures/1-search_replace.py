53783c69067dbb32f0e681bb64c48478e3f5efd5
Abdurrahman <dreydare16@gmail.com>
2023-03-15 13:16:58 +0100

    Fixing error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    i = 0
-    for item in my_list:
-        if item == search:
-            item[i] = replace
-    i += 1
-    return my_list
+    return list(map(lambda x; x.replace(search, replace), my_list))
