02a289e708fea43cf0893a50388c3857dbb63c4c
Abdurrahman <dreydare16@gmail.com>
2023-03-15 15:31:43 +0100

    included a logical operator

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,6 +2,6 @@
 def common_elements(set_1, set_2):
     new_set = []  
     for item in set_1:
-        if item in set_2:
+        if item in set_2 and item in set_1:
             new_set.append(item)
     return set(new_set) 
